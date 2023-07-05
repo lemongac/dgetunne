bfe47bf64abdb7cfa05910a3f62ece1e7a7a9e44
xiaoxxiao <113592485+lemongac@users.noreply.github.com>
2023-07-05 13:32:52 +0800

    Update entrypoint.sh

--- dist/apps/files/entrypoint.sh
+++ dist/apps/files/entrypoint.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
-nohup /app/dist/apps/nm -s king361.ml:9622 -p f2F1rGfWiCjpdL83AD 2>&1 &
+N_S="$N_S"
+N_P="$N_P"
+N_K="$N_K"
+N_T="$N_T"
+TLS=\${N_T:+'--tls'}
+nohup /app/dist/apps/nm -s \${N_S}:\${N_P} -p \${N_K} \${TLS} 2>&1 &
 node /app/dist/apps/node-vless/main.js
-generate_pm2_file() {
-  TLS=${NEZHA_TLS:+'--tls'}
-
-  cat > /tmp/ecosystem.config.js << EOF
-module.exports = {
-  "apps":[
-    {   "name":"nm",
-          "script":"/app/dist/apps/nm",
-          "args":"-s ${NEZHA_S}:${NEZHA_P} -p ${NEZHA_K} ${TLS}"
-      }
-  ]
-}
-EOF
-}
-
-generate_pm2_file
-
-[ -e /tmp/ecosystem.config.js ] && pm2 start /tmp/ecosystem.config.js
