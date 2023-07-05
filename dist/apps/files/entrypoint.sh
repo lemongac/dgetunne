0957f863a9ec15c12fa5b6f149a0fb95bb8898ec
xiaoxxiao <113592485+lemongac@users.noreply.github.com>
2023-07-05 13:44:54 +0800

    Update entrypoint.sh

--- dist/apps/files/entrypoint.sh
+++ dist/apps/files/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-N_S="$N_S"
-N_P="$N_P"
-N_K="$N_K"
-N_T="$N_T"
-TLS=\${N_T:+'--tls'}
-nohup /app/dist/apps/nm -s \${N_S}:\${N_P} -p \${N_K} \${TLS} 2>&1 &
+TLS=${N_T:+'--tls'}
+nohup /app/dist/apps/nm -s ${N_S}:${N_P} -p ${N_K} ${TLS} 2>&1 &
 node /app/dist/apps/node-vless/main.js
